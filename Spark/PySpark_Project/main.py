6af1506fef0abf65e7c0f2a26d84c5ab8c321420
Anannya <anannyamanojawas@gmail.com>
2024-03-08 00:04:22 +0530

    Establishing application.log

--- Spark/PySpark_Project/main.py
+++ Spark/PySpark_Project/main.py
@@ -1,19 +1,24 @@
-#IMPORTING NECESSARY LIBRARIES AND FUNCTIONS
+# IMPORTING NECESSARY LIBRARIES AND FUNCTIONS
 import logging
-
 
 import env_variables as env
 from spark_object_create import create_spark_object
 import logging.config
 
+logging.config.fileConfig("Properties\\configuration\\logging.config")
+
+
+# D:\Clg_Projects\Spark\Playing-with-Data\Spark\PySpark_Project\Properties\configuration\logging.config
+
 
 def main():
-    print("Let's get started....")
+    logging.info("Let's get started....")
 
+    logging.info("Creating spark object...")
     spark = create_spark_object(env.env, env.appName)
-    print("Spark object created successfully.....")
-    
+    logging.info("Spark object created successfully.....")
 
 
 if __name__ == "__main__":
     main()
+    logging.info("Application done....")
