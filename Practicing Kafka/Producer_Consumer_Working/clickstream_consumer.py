ae86cf149e083c9cb4fb7faa4f9658ce270a9b1d
Anannya Manojawas <85965261+Anannya-M@users.noreply.github.com>
2024-03-14 23:53:57 +0530

    Update clickstream_consumer.py

--- Practicing Kafka/Producer_Consumer_Working/clickstream_consumer.py
+++ Practicing Kafka/Producer_Consumer_Working/clickstream_consumer.py
@@ -3,14 +3,14 @@ import json
 from kafka_config import KafkaConfig
 import pandas as pd
 
-
+# Creating function for initializing kafka consumer
 def create_kafka_consumer(topic_name):
     # Set up a Kafka consumer with specified topic and configurations
     consumer = KafkaConsumer(
         topic_name,
         bootstrap_servers=KafkaConfig.BOOTSTRAP_SERVERS,
         auto_offset_reset='earliest',
-        value_deserializer=lambda res: json.loads(res.decode('utf-8')))
+        value_deserializer=lambda res: json.loads(res.decode('utf-8')))   # Loading all the data from the kafka topic
     return consumer
 
 
@@ -27,14 +27,14 @@ def fetch_data_from_kafka(consumer):
 
 
 def main():
-    # Fetch data from Kafka on aggregated turnout by location
-    location_consumer = create_kafka_consumer(KafkaConfig.CLICKSTREAM_TOPIC)
-    location_data = fetch_data_from_kafka(location_consumer)
-    location_result = pd.DataFrame(location_data)
+    # Fetch data from Kafka on clickstream_events
+    location_consumer = create_kafka_consumer(KafkaConfig.CLICKSTREAM_TOPIC) # Storing the loaded data to 'location_consumer'
+    location_data = fetch_data_from_kafka(location_consumer) 
+    location_result = pd.DataFrame(location_data) # converting the fetched data into a pandas dataframe
 
     return location_result
 
 
 if __name__ == '__main__':
-    data = main()
+    data = main() # Returns a dataframe 
     print(data)
